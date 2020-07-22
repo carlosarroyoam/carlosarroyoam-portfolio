52ebaf3678b5cc73e5bcd991e7e95c57ff9c81a5
Carlos Alberto Arroyo Martinez <carlosarroyoam@gmail.com>
2020-07-21 22:41:28 -0500

    Added: @nuxt/content.

---
+++ pages/blog/article/_slug.vue
@@ -0,0 +1,47 @@
+<template>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-sm-12 col-md-10 col-lg-8 mx-auto">
+				<article>
+					<img :src="article.img" :alt="article.alt" />
+					<p>{{ formatDate(article.updatedAt) }}</p>
+
+					<nuxt-content :document="article" />
+
+					<author :author="article.author" />
+				</article>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+import Author from '../../../components/globals/author';
+
+export default {
+	name: 'Article',
+	layout: 'app',
+	components: {
+		Author
+	},
+	methods: {
+		formatDate(date) {
+			const options = { year: 'numeric', month: 'long', day: 'numeric' };
+			return new Date(date).toLocaleDateString('es-mx', options);
+		}
+	},
+	head() {
+		return {
+			title: this.article.title,
+			meta: [{ hid: 'description', name: 'description', content: this.article.description }]
+		};
+	},
+	async asyncData({ $content, params }) {
+		const article = await $content('articles', params.slug).fetch();
+		return { article };
+	}
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
