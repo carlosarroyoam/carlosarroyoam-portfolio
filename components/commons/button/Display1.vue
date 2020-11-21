f76d79416d2d8d80707a1321d204d25d09a26f38
Carlos Alberto Arroyo Martinez <carlosarroyoam@gmail.com>
2020-11-21 16:57:54 -0600

    chore(components): updated mobile buttons and text sizes

--- components/commons/button/Display1.vue
+++ components/commons/button/Display1.vue
@@ -1,5 +1,5 @@
 <template>
-	<h1 class="max-w-4xl text-4xl font-normal leading-tight text-gray-900 uppercase sm:text-5xl font-display">
+	<h1 class="max-w-4xl text-3xl font-normal leading-tight text-gray-900 uppercase sm:text-4xl lg:text-5xl font-display">
 		<slot></slot>
 	</h1>
 </template>
